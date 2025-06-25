7010859c015f965e656e16b2e8bfaa629a48ef06
Muhammad Saqib <i190494@nu.edu.pk>
2025-06-25 22:31:51 +0200

    adding comments and refactorting the code for representation extraction

--- embedding_extraction_from_encoder_api.py
+++ embedding_extraction_from_encoder_api.py
@@ -12,16 +12,11 @@ import time
 import torchvision.transforms as T
 import torchvision.transforms.functional as TF
 
-token = "54614611"
-chunk_size = 1000
-max_queries = 100000
-
-answer = {'seed': 18632002, 'port': '9054'}
-# save the values
-seed = int(answer['seed'])
-port = str(answer['port'])
-
-print(f"Using SEED={seed}, PORT={port}")
+TOKEN = "54614611"
+CHUNK_SIZE = 1000
+MAX_QUERIES = 100000
+SEED = "18632002"
+PORT = "9054"
 
 class TaskDataset(Dataset):
     def __init__(self, transform=None):
@@ -41,20 +36,6 @@ class TaskDataset(Dataset):
     def __len__(self):
         return len(self.ids)
 
-# Load dataset
-data_simple = torch.load("ModelStealingPub.pt", weights_only=False)
-data_aug = torch.load("ModelStealingPub_augmented.pt", weights_only=False)
-
-imgs = data_simple.imgs + data_aug.imgs
-
-mean = [0.2980, 0.2962, 0.2987]
-std = [0.2886, 0.2875, 0.2889]
-
-transform = T.Compose([
-    T.ToTensor(),
-    T.Normalize(mean, std)
-])
-
 
 # De-normalize to bring back to [0,1] range for conversion to PIL
 def denormalize(tensor, mean, std):
@@ -62,11 +43,6 @@ def denormalize(tensor, mean, std):
         t.mul_(s).add_(m)
     return torch.clamp(tensor, 0, 1)
 
-indices = np.arange(len(imgs))
-# Shuffle indices reproducibly
-rng = np.random.default_rng(seed)
-shuffled_indices = indices.copy()
-rng.shuffle(shuffled_indices)
 
 def query_api(img_list, port):
     url = f"http://34.122.51.94:{port}/query"
@@ -85,8 +61,28 @@ def query_api(img_list, port):
         raise RuntimeError(f"API error: {resp.status_code}, {resp.text}")
 
 
+print(f"Using SEED={SEED}, PORT={PORT}")
+# Load dataset
+data_simple = torch.load("ModelStealingPub.pt", weights_only=False)
+data_aug = torch.load("ModelStealingPub_augmented.pt", weights_only=False)
+imgs = data_simple.imgs + data_aug.imgs
+
+# Shuffle indices reproducibly
+indices = np.arange(len(imgs))
+rng = np.random.default_rng(seed)
+shuffled_indices = indices.copy()
+rng.shuffle(shuffled_indices)
+
+# Normalization
+mean = [0.2980, 0.2962, 0.2987]
+std = [0.2886, 0.2875, 0.2889]
+transform = T.Compose([
+    T.ToTensor(),
+    T.Normalize(mean, std)
+])
+
 all_reps, all_idx = [], []
-# Process in shuffled order
+# Process in shuffled order to extract api encodings
 for start in tqdm(range(0, min(len(imgs), max_queries), chunk_size)):
     batch_idx = shuffled_indices[start:start+chunk_size]
     batch_imgs = [imgs[i] for i in batch_idx]
@@ -96,7 +92,10 @@ for start in tqdm(range(0, min(len(imgs), max_queries), chunk_size)):
     all_reps.extend(reps)
     all_idx.extend(batch_idx.tolist())
 
-    # Save progress
+    """
+    Save progress after each iteration so that even if we have some error in later iteration, we won't lose the data encodings already extracted.
+    This is due to that fact that we have 100k queries limit. Ensuring we don't exhaust that 
+    """ 
     with open("embeddings.pickle", "wb") as f:
         pickle.dump({"indices": all_idx, "reps": all_reps}, f)
 
